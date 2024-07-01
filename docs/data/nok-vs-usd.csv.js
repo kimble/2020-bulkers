92b54bb5780e380f97ef0a49ded6a5f924f72459
Kim A. Betti <kim.betti@gmail.com>
2024-07-01 19:37:31 +0200

    Initial commit with data loader for nok/usd

---
+++ docs/data/nok-vs-usd.csv.js
@@ -0,0 +1,23 @@
+import {csvFormat, tsvParse, dsvFormat} from "d3-dsv";
+
+async function text(url) {
+    const response = await fetch(url);
+    if (!response.ok) throw new Error(`fetch failed: ${response.status}`);
+    return response.text();
+}
+
+// Norges Bank has a lovely API for this!
+// https://app.norges-bank.no/query/index.html#/no/currency
+const uri = "https://data.norges-bank.no/api/data/EXR/B.USD.NOK.SP?apisrc=qb&format=csv&startPeriod=2020-01-01&endPeriod=2024-07-01&locale=no&bom=include";
+const raw = await text(uri);
+
+const parser = dsvFormat(";")
+
+const parsed = parser.parse(raw, (d) => {
+    return {
+        date: d["TIME_PERIOD"],
+        value: parseFloat(d["OBS_VALUE"].replace(",", "."))
+    }
+});
+
+process.stdout.write(csvFormat(parsed));
---
+++ docs/experiment.md
@@ -0,0 +1,13 @@
+---
+title: Experiment
+---
+
+
+```js
+const nokusd = await FileAttachment("data/nok-vs-usd.csv").csv({ typed: true });
+```
+
+
+```js
+display(nokusd);
+```
